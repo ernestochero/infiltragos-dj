251e0f54dd9bafad25be6196569009f255cdc73c
jpalcalde <jpalcalde72@gmail.com>
2025-09-18 12:47:31 -0500

    fix: add validation and handle for sessionStorage

--- src/app/components/FirstVisitModal.tsx
+++ src/app/components/FirstVisitModal.tsx
@@ -1,37 +1,39 @@
 import { Dialog } from "@headlessui/react";
-import { useEffect, useState, ReactNode } from "react";
+import { useEffect, useState } from "react";
 
 interface FirstVisitModalProps {
   storageKey?: string;
   title?: string;
   description?: string;
   buttonText?: string;
-  children?: ReactNode;
 }
 
 export default function FirstVisitModal({
   storageKey = "welcomeSession",
-  title = "Bienvenido 🍸",
-  description = "Gracias por visitar nuestra app 🚀",
+  title = "Bienvenido",
+  description = "Gracias por visitarnos",
   buttonText = "Cerrar",
-  children,
 }: FirstVisitModalProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    const alreadyVisited = sessionStorage.getItem(storageKey);
-    if (!alreadyVisited) {
-      setIsOpen(true);
-      sessionStorage.setItem(storageKey, "true");
+    if (typeof window !== "undefined") {
+      const alreadyVisited = sessionStorage.getItem(storageKey);
+      if (!alreadyVisited) {
+        setIsOpen(true);
+      }
     }
   }, [storageKey]);
 
+  function handleClose() {
+    setIsOpen(false);
+    if (typeof window !== "undefined") {
+      sessionStorage.setItem(storageKey, "true");
+    }
+  }
+
   return (
-    <Dialog
-      open={isOpen}
-      onClose={() => setIsOpen(false)}
-      className="relative z-50"
-    >
+    <Dialog open={isOpen} onClose={handleClose} className="relative z-50">
       <div className="fixed inset-0 bg-black/80 flex items-center justify-center">
         <div className="bg-white p-6 m-3 rounded-2xl shadow-xl max-w-md w-full">
           <h2 className="text-3xl font-bold text-center text-indigo-600">
@@ -40,7 +42,6 @@ export default function FirstVisitModal({
           <p className="mt-3 text-xl text-gray-800 text-center whitespace-pre-line">
             {description}
           </p>
-          {children && <div className="mt-4">{children}</div>}
           <div className="mt-6 flex justify-center">
             <button
               className="text-xl px-6 py-2 rounded-lg bg-indigo-600 text-white font-medium hover:bg-indigo-700 transition"
